438d8b958d75932f5a32cd988c4a94274f3afd95
kasra mehralizadeh <136126620+KasraMehr@users.noreply.github.com>
2025-02-20 18:02:19 -0500

    Update redisClient.js

--- redisClient.js
+++ redisClient.js
@@ -1,6 +1,6 @@
 const Redis = require("ioredis");
 
-const redisUri = process.env.REDIS_URI || "redis://127.0.0.1:6379";
+const redisUri = process.env.REDIS_URI || "redis://:IYzjt8tFofqdHfNH4Ryiw2mU@dictionary-redis:6379/0";
 const redis = new Redis(redisUri);
 
 redis.on("connect", () => console.log("Connected to Redis!"));
