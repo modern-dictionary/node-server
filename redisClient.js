ce1dcb4832169ab0fb857c30ebbd173e4d5feb61
KasraMehr <kasramehrali@gmail.com>
2025-02-20 17:42:57 -0500

    fixed redis

--- redisClient.js
+++ redisClient.js
@@ -1,11 +1,9 @@
 const Redis = require("ioredis");
 
-const redis = new Redis({
-  host: "127.0.0.1",
-  port: 6379,
-});
+const redisUri = process.env.REDIS_URI || "redis://127.0.0.1:6379";
+const redis = new Redis(redisUri);
 
 redis.on("connect", () => console.log("Connected to Redis!"));
-redis.on("error", (err) => console.log("Redis Error: ", err));
+redis.on("error", (err) => console.error("Redis Error:", err));
 
 module.exports = redis;
